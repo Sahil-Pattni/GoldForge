cc08be54c2afbfaa14ee95ca79ef6df5095ee41f
Sahil Pattni <sahilpattni97@gmail.com>
2023-08-22 16:18:26 +0400

    Added home page

---
+++ src/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("GoldForge")
+st.subheader(
+    "Welcome to GoldForge, a web app that powers the precision jewelry manufacturing."
+)
+st.caption("© 2023 Sahil Pattni. All rights reserved.")
