af3689bbbc0ecae7983505d0a14c5b8d5aed9827
Sahil Pattni <sahilpattni97@gmail.com>
2023-08-23 10:28:55 +0400

    Revert "Disabled bs4"
    This reverts commit 7ef4dc77ce40469562906c1e5421ddc5bb0d7978.

--- src/Home.py
+++ src/Home.py
@@ -1,31 +1,30 @@
 from typing import Tuple
 import streamlit as st
 import requests
+import bs4
 
-# import bs4
 
+def london_fix() -> Tuple[float, float]:
+    """
+    Scrapes the London Fix price of gold.
 
-# def london_fix() -> Tuple[float, float]:
-#     """
-#     Scrapes the London Fix price of gold.
+    Returns:
+        am_price (float): The AM price of gold.
+        pm_price (float): The PM price of gold.
+    """
 
-#     Returns:
-#         am_price (float): The AM price of gold.
-#         pm_price (float): The PM price of gold.
-#     """
+    url = "https://www.kitco.com/gold.londonfix.html"
+    res = requests.get(url)
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    price_sheet = soup.find("tr", {"class": "even"})
 
-#     url = "https://www.kitco.com/gold.londonfix.html"
-#     res = requests.get(url)
-#     soup = bs4.BeautifulSoup(res.text, "html.parser")
-#     price_sheet = soup.find("tr", {"class": "even"})
+    am_price = float(price_sheet.find("td", {"class": "am"}).text)
+    pm_price = float(price_sheet.find("td", {"class": "pm"}).text)
 
-#     am_price = float(price_sheet.find("td", {"class": "am"}).text)
-#     pm_price = float(price_sheet.find("td", {"class": "pm"}).text)
+    date = soup.find("td", {"class": "dates"}).text.upper()
+    date = date.replace("QUOTES AS OF", "").strip().capitalize()
 
-#     date = soup.find("td", {"class": "dates"}).text.upper()
-#     date = date.replace("QUOTES AS OF", "").strip().capitalize()
-
-#     return am_price, pm_price, date
+    return am_price, pm_price, date
 
 
 st.title("GoldForge")
@@ -43,28 +42,28 @@ with st.sidebar:
 st.divider()
 
 
-# am_price, pm_price, date = london_fix()
+am_price, pm_price, date = london_fix()
 
-# st.header("London Fix Gold Price")
-# st.caption(f"Last updated: {date}")
-# col1, col2 = st.columns(2)
-# with col1:
-#     st.subheader("AM")
-#     st.metric(
-#         "Gold Price ($/oz)",
-#         f"${am_price:,.2f}",
-#     )
-#     st.metric(
-#         "Gold Price (AED / kg)",
-#         f"{(am_price/31.1035) * 1000 * 3.67:,.0f} AED",
-#     )
-# with col2:
-#     st.subheader("PM")
-#     st.metric(
-#         "Gold Price ($/oz)",
-#         f"${pm_price:,.2f}",
-#     )
-#     st.metric(
-#         "Gold Price (AED / kg)",
-#         f"{(pm_price/31.1035) * 1000 * 3.67:,.0f} AED",
-#     )
+st.header("London Fix Gold Price")
+st.caption(f"Last updated: {date}")
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("AM")
+    st.metric(
+        "Gold Price ($/oz)",
+        f"${am_price:,.2f}",
+    )
+    st.metric(
+        "Gold Price (AED / kg)",
+        f"{(am_price/31.1035) * 1000 * 3.67:,.0f} AED",
+    )
+with col2:
+    st.subheader("PM")
+    st.metric(
+        "Gold Price ($/oz)",
+        f"${pm_price:,.2f}",
+    )
+    st.metric(
+        "Gold Price (AED / kg)",
+        f"{(pm_price/31.1035) * 1000 * 3.67:,.0f} AED",
+    )
